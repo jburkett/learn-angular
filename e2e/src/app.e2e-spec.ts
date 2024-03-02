ae72d165ae1def2504aa5e75524fc6e309dc92ca
jburkett <1856722+jburkett@users.noreply.github.com>
2024-03-01 20:25:08 -0600

    start lesson 3 (#2)

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,11 +1,20 @@
 import { browser, element, by, logging } from 'protractor';
 
-describe('first-app-lesson-01 app', () => {
+describe('first-app-lesson-02 app', () => {
 
   beforeEach(() => browser.get(''));
 
   it('should display correct title', async () => {
-    expect(await element.all(by.css('h1')).get(0).getText()).toEqual('Hello world!');
+    expect(await element.all(by.css('.brand-logo')).get(0).getAttribute('src')).toContain('/assets/logo.svg');
+  });
+
+  it('should have a filter string input', async () => {
+    expect(await element.all(by.css('input')).get(0).getAttribute('placeholder')).toEqual('Filter by city');
+    expect(await element.all(by.css('input')).get(0).getAttribute('type')).toEqual('text');
+  });
+
+  it('should have a search button', async () => {
+    expect(await element.all(by.css('button')).get(0).getText()).toEqual('Search');
   });
 
   afterEach(async () => {
